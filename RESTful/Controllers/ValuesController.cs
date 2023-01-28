a2fd3460e2b14f0da43912483fc65f3a6ed3b0e6
İlker <117676317+ilkersatur@users.noreply.github.com>
2023-01-28 19:36:06 +0300

    Add files via upload

---
+++ RESTful/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTful.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //public string Get()
+        //{
+        //    return "value";
+        //}
+
+        [HttpGet]
+        public string[] VerileriGetir()
+        {
+            return new string[] { "Istanbul", "Ankara", "İzmir" };
+        }
+    }
+}
